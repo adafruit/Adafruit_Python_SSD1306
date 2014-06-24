0466cf7d03eb4cb3b5c63779897395aba00ec564
Tony DiCola <tony@tonydicola.com>
2014-06-24 13:04:47 -0700

    Add example of i2c bus config and software SPI.

--- examples/shapes.py
+++ examples/shapes.py
@@ -48,11 +48,20 @@ disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 # 128x64 display with hardware I2C:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
 
+# Alternatively you can specify an explicit I2C bus number, for example
+# with the 128x32 display you would use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
+
 # 128x32 display with hardware SPI:
 # disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
 
 # 128x64 display with hardware SPI:
 # disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Alternatively you can specify a software SPI implementation by providing
+# digital GPIO pin numbers for all the required display pins.  For example
+# on a Raspberry Pi with the 128x32 display you might use:
+# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
 
 # Initialize library.
 disp.begin()
