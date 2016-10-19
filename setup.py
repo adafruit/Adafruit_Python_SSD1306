902e762c30a3374eaa8ff19b948bde8b371b162f
Tony DiCola <tony@tonydicola.com>
2016-10-19 22:08:52 +0000

    Fix python 3 pip install issue with ez_setup relative import.

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
-from ez_setup import use_setuptools
-use_setuptools()
+try:
+    # Try using ez_setup to install setuptools if not already installed.
+    from ez_setup import use_setuptools
+    use_setuptools()
+except ImportError:
+    # Ignore import error and assume Python 3 which already has setuptools.
+    pass
+
 from setuptools import setup, find_packages
 
 classifiers = ['Development Status :: 4 - Beta',
@@ -12,7 +18,7 @@ classifiers = ['Development Status :: 4 - Beta',
                'Topic :: System :: Hardware']
 
 setup(name              = 'Adafruit_SSD1306',
-      version           = '1.6.0',
+      version           = '1.6.1',
       author            = 'Tony DiCola',
       author_email      = 'tdicola@adafruit.com',
       description       = 'Python library to use SSD1306-based 128x64 or 128x32 pixel OLED displays with a Raspberry Pi or Beaglebone Black.',
